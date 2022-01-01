73be05deac4e4eb3d765ae64046b24a4141848e6
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-01-01 01:22:27 +0100

    add basic 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,14 @@
+import Metadata from "components/common/Metadata";
+
+const Error404 = () => {
+  return (
+    <>
+      <Metadata title={"Not Found"} />
+      <h1>404</h1>
+      <h6>{"Oh no! Seems like you've fallen into the void"}</h6>
+      <p>{"Get back to safety before it's too late"}</p>
+    </>
+  );
+};
+
+export default Error404;
