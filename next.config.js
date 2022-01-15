f6027a4cb10e1baf989c7385a65033ce1f3dc8f4
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-01-16 00:17:31 +0100

    setup svgr

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
     defaultLocale: "en",
   },
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
