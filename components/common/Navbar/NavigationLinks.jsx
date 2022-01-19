f35752008cc1d0aca7772d0e605198aff6864d7c
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-01-19 07:18:34 +0100

    stylize nav links

--- components/common/Navbar/NavigationLinks.jsx
+++ components/common/Navbar/NavigationLinks.jsx
@@ -6,7 +6,7 @@ const NavigationLinks = ({ links }) => {
       {links.map((link, index) => {
         return (
           <Link key={index} href={link.url}>
-            <a className="text-grey-50 hover:text-grey-50 mt-2 transition-colors duration-200 transform lg:mt-0 lg:mx-4 ">
+            <a className="mt-2 font-semibold transition-colors duration-200 transform text-neutral-700 dark:text-neutral-200 hover:text-violet-600 dark:hover:text-violet-500 lg:mt-0 lg:mx-4">
               {link.text}
             </a>
           </Link>
