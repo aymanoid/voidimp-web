30a236c43eb15fb2711e0e13b4ac87c9c8f221a0
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-01-20 14:33:11 +0100

    add layout to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,18 @@
-const Index = () => {
-  return <h1>haha</h1>;
+import Layout from "components/common/Layout";
+import { getGlobalData } from "utils/queries";
+
+const Index = ({ globalData }) => {
+  return <Layout globalData={globalData}>haha</Layout>;
+};
+
+export const getStaticProps = async ({ locale }) => {
+  const [globalData] = await Promise.all([getGlobalData(locale)]);
+
+  return {
+    props: {
+      globalData,
+    },
+  };
 };
 
 export default Index;
