7db0c55117bba665ed58d220d69058ef9cd0ee5c
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-01-29 20:21:25 +0100

    add link of test article to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,19 @@
 import Layout from "components/common/Layout";
 import { getGlobalData } from "utils/queries";
+import Link from "next/link";
 
 const Index = ({ globalData }) => {
-  return <Layout globalData={globalData}>haha</Layout>;
+  return (
+    <Layout globalData={globalData}>
+      <div>haha</div>
+      <Link href="/articles/this-post-is-for-testing-purposes-only-theres-nothing-interesting-here-to-read">
+        <a>
+          This Post Is for Testing Purposes Only, There’s Nothing Interesting
+          Here To Read
+        </a>
+      </Link>
+    </Layout>
+  );
 };
 
 export const getStaticProps = async ({ locale }) => {
