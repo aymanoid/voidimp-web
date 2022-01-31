3929a3256c251e8cdf54d765d5f0cf746ea447da
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-01-31 07:48:16 +0100

    tailwind classes sort

--- pages/404.jsx
+++ pages/404.jsx
@@ -7,9 +7,9 @@ const FourZeroFour = ({ globalData, seoData, pageData }) => {
   return (
     <Layout globalData={globalData}>
       <PageSEO seoData={seoData} />
-      <div className="container px-6 py-16 mx-auto text-center">
-        <div className="max-w-lg mx-auto">
-          <h1 className="text-violet-500 text-6xl font-extrabold leading-9 tracking-tight md:text-8xl mb-8 md:leading-14 md:px-6">
+      <div className="container mx-auto px-6 py-16 text-center">
+        <div className="mx-auto max-w-lg">
+          <h1 className="md:leading-14 mb-8 text-6xl font-extrabold leading-9 tracking-tight text-violet-600 dark:text-violet-500 md:px-6 md:text-8xl">
             404
           </h1>
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
@@ -17,7 +17,7 @@ const FourZeroFour = ({ globalData, seoData, pageData }) => {
           </p>
           <p className="mb-8">{pageData.secondText}</p>
           <Link href="/">
-            <a className="bg-transparent hover:bg-violet-500 font-semibold py-2 px-4 border border-violet-500 hover:border-transparent rounded-full">
+            <a className="rounded-full border border-violet-600 bg-transparent px-4 py-2 font-semibold hover:border-transparent hover:bg-violet-600 dark:border-violet-500 dark:hover:bg-violet-500">
               {pageData.backButton}
             </a>
           </Link>
