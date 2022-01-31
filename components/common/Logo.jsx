8f51ceb7d0134ed886e28b24eba6179f387d7d52
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-01-31 07:47:36 +0100

    use next/link in logo component

--- components/common/Logo.jsx
+++ components/common/Logo.jsx
@@ -1,18 +1,21 @@
+import Link from "next/link";
 import Image from "next/image";
 
 const Logo = () => {
   return (
     <div>
-      <a href="/">
-        <span>
-          <Image
-            src="/logo.svg"
-            alt="VoidImp Logo"
-            width={1906.2 / 14}
-            height={430.55 / 14}
-          />
-        </span>
-      </a>
+      <Link href="/">
+        <a>
+          <span>
+            <Image
+              src="/logo.svg"
+              alt="VoidImp Logo"
+              width={1906.2 / 14}
+              height={430.55 / 14}
+            />
+          </span>
+        </a>
+      </Link>
     </div>
   );
 };
