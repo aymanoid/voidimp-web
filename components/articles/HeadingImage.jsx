3505f70d7cf2ad9db943530fa4837d432920bdb5
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-01 01:37:50 +0100

    handle nullable fields

--- components/articles/HeadingImage.jsx
+++ components/articles/HeadingImage.jsx
@@ -5,12 +5,11 @@ const HeadingImage = ({ imageData }) => {
     <div className="flex justify-center">
       <div className="flex rounded-3xl border border-violet-600/50 p-2 dark:border-violet-500/50">
         <Image
-          src={imageData.url}
-          alt={imageData.alt}
-          width={imageData.dimensions.width / 3}
-          height={imageData.dimensions.height / 3}
+          src={imageData?.url}
+          alt={imageData?.alt}
+          width={imageData?.dimensions?.width / 3}
+          height={imageData?.dimensions?.height / 3}
           className="rounded-3xl"
-          priority
         />
       </div>
     </div>
