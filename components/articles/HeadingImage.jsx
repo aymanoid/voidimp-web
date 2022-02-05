35a6986b5a1354590611276cc220a64bd3085fa0
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-05 22:32:13 +0100

    use imgix component in heading image

--- components/articles/HeadingImage.jsx
+++ components/articles/HeadingImage.jsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Imgix from "react-imgix";
 
 const HeadingImage = ({ imageData }) => {
   return (
     <div className="flex justify-center">
       <div className="flex rounded-3xl border border-violet-600/50 p-2 dark:border-violet-500/50">
-        <Image
+        <Imgix
           src={imageData?.url}
           alt={imageData?.alt}
           width={imageData?.dimensions?.width / 3}
