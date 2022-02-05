10e3f64de2dc723fc2e6bb0e7c461a873277abf5
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-05 02:06:26 +0100

    polish author info

--- components/articles/AuthorInfo.jsx
+++ components/articles/AuthorInfo.jsx
@@ -11,9 +11,9 @@ const AuthorInfo = ({ authorData }) => {
   }[locale];
 
   return (
-    <div className="mt-6 flex">
+    <div className="group mt-6 flex">
       <Link href={`/authors/${authorData.username}`}>
-        <a className="flex items-center space-x-2">
+        <a className="flex items-center ">
           <div className="relative inline-flex h-12 w-12 flex-shrink-0 items-center justify-center overflow-hidden text-lg font-semibold uppercase sm:h-24 sm:w-24 sm:text-xl ">
             <Image
               src={authorData.avatarThumbnail.url}
@@ -25,12 +25,12 @@ const AuthorInfo = ({ authorData }) => {
           </div>
         </a>
       </Link>
-      <div className="ml-3 flex max-w-lg flex-col rtl:mr-3 sm:ml-5">
+      <div className="flex max-w-lg flex-col ltr:ml-3 rtl:mr-3 ltr:sm:ml-5 rtl:sm:ml-5">
         <span className="text-xs uppercase tracking-wider text-neutral-400">
           {strings.writtenBy}
         </span>
-        <h2 className="text-lg font-semibold text-neutral-900 dark:text-neutral-200">
-          <Link href={`/authors/${authorData?.username}`}>
+        <h2 className="text-lg font-semibold text-neutral-900 hover:text-violet-600 dark:text-neutral-200 dark:hover:text-violet-500">
+          <Link href={`/authors/${authorData.username}`}>
             <a>{authorData.displayName}</a>
           </Link>
         </h2>
