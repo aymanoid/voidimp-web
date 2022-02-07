68f5e7f01e000e8c7c486d49d8da7159305eb934
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-07 01:23:23 +0100

    improve heading image responsiveness

--- components/articles/HeadingImage.jsx
+++ components/articles/HeadingImage.jsx
@@ -1,16 +1,41 @@
-import Imgix from "react-imgix";
+import Imgix, { Picture, Source } from "react-imgix";
 
 const HeadingImage = ({ imageData }) => {
   return (
     <div className="flex justify-center">
       <div className="flex rounded-3xl border border-violet-600/50 p-2 dark:border-violet-500/50">
-        <Imgix
-          src={imageData?.url}
-          alt={imageData?.alt}
-          width={imageData?.dimensions?.width / 3}
-          height={imageData?.dimensions?.height / 3}
-          className="rounded-3xl"
-        />
+        <figure>
+          <Picture>
+            <Source
+              src={imageData.url}
+              width={640}
+              htmlAttributes={{ media: "(min-width: 1024px)" }}
+            />
+            {/* <Source
+              src={imageData.url}
+              width={768}
+              htmlAttributes={{ media: "(min-width: 768px)" }}
+            /> */}
+            <Source
+              src={imageData.url}
+              width={768}
+              htmlAttributes={{ media: "(min-width: 640px)" }}
+            />
+            {/* <Source
+              src={imageData.url}
+              width={640}
+              htmlAttributes={{ media: "(min-width: 0px)" }}
+            /> */}
+            <Imgix
+              className="rounded-3xl"
+              src={imageData.url}
+              width={640}
+              height={360}
+              htmlAttributes={{ alt: imageData.alt }}
+            />
+            {/* TODO: add and stylize figurecaption tag */}
+          </Picture>
+        </figure>
       </div>
     </div>
   );
