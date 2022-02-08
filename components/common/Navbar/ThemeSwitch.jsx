e460ef492f6129d8448c76b77393cf1f39f5d8cc
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-08 20:31:46 +0100

    add aria labels to buttons

--- components/common/Navbar/ThemeSwitch.jsx
+++ components/common/Navbar/ThemeSwitch.jsx
@@ -1,13 +1,21 @@
+import { useRouter } from "next/router";
 import { useTheme } from "next-themes";
 import SunSvg from "components/icons/sun.svg";
 import MoonSvg from "components/icons/moon.svg";
 
 const ThemeSwitch = () => {
+  const { locale } = useRouter();
   const { theme, setTheme } = useTheme();
+
   const IconSvg = theme === "dark" ? SunSvg : MoonSvg;
+  const strings = {
+    en: { btnAriaLabel: "Switch Theme" },
+    ar: { btnAriaLabel: "غير السمة" },
+  }[locale];
 
   return (
     <button
+      aria-label={strings.btnAriaLabel}
       className="mx-2 transform text-neutral-700 transition-colors duration-200 hover:text-violet-600 dark:text-neutral-200 dark:hover:text-violet-500 lg:mt-0"
       onClick={() => {
         setTheme(theme === "light" ? "dark" : "light");
