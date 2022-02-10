f87f4f1ffeedb0d6e026443a482e3ce762602abc
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-10 12:28:54 +0100

    improvement and add default data

--- utils/queries/get-author-data.js
+++ utils/queries/get-author-data.js
@@ -7,16 +7,43 @@ const localeMap = {
   "ar-ma": "ar",
 };
 
-const getAuthorData = async (username, locale) => {
-  const response = await client.getByUID("author", username, {
-    lang: localeMap[locale],
-  });
+const defaultData = {
+  en: {
+    displayName: "No Display Name",
+    avatar: {
+      url: "https://images.unsplash.com/photo-1608889175123-8ee362201f81",
+      alt: "Author Avatar",
+    },
+    bio: "No bio has been set.",
+  },
+  ar: {
+    displayName: "لا يوجد اسم عرض",
+    avatar: {
+      url: "https://images.unsplash.com/photo-1608889175123-8ee362201f81",
+      alt: "صورة الكاتب",
+    },
+    bio: "لم يتم تعيين السيرة الذاتية.",
+  },
+};
 
-  const data = {
-    displayName: response.data.display_name,
-    avatarThumbnail: response.data.avatar.thumbnail,
-    bio: response.data.bio,
-  };
+const getAuthorData = async (username, locale) => {
+  let data;
+  try {
+    const response = await client.getByUID("author", "username", {
+      lang: localeMap[locale],
+    });
+
+    data = {
+      displayName:
+        response.data.display_name || defaultData[locale].displayName,
+      avatar: response.data.avatar || defaultData[locale].avatar,
+      bio: response.data.bio || defaultData[locale].bio,
+    };
+  } catch {
+    data = defaultData[locale];
+  }
+
+  delete data.avatar.dimensions;
 
   return data;
 };
