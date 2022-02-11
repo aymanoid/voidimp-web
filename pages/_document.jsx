846e1cac9fe12b814da5c74138599018a54d6cfb
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-11 02:04:53 +0100

    preconnect/prefetch images cdn for better performance (?)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,8 +6,10 @@ class VoidImpDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.svg" />
+          <link rel="preconnect" href="https://images.prismic.io" />
+          <link rel="dns-prefetch" href="https://images.prismic.io" />
         </Head>
-        <body className="antialiased bg-white dark:bg-neutral-900">
+        <body className="bg-white antialiased dark:bg-neutral-900">
           <Main />
           <NextScript />
         </body>
