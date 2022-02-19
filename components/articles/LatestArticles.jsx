a5c044befc40eb3639bf0d35affae9860d54a83e
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-19 04:52:37 +0100

    fix urls

--- components/articles/LatestArticles.jsx
+++ components/articles/LatestArticles.jsx
@@ -5,51 +5,35 @@ import Image from "next/image";
 const articlesData = [
   {
     headline: "TikTok Cosplayer Yandere Freak Charged With Manslaughter",
-    url: "/haha1",
+    slug: "haha1",
     thumbnail: {
       url: "https://images.unsplash.com/photo-1615961943966-ab7585d5bf4e",
       alt: "s1",
-      dimensions: {
-        width: 1920,
-        height: 1080,
-      },
     },
   },
   {
     headline:
       "Black Myth: Wukong New Year Trailer Reveals Cats Can Be Difficult Mo-Cap Actors",
-    url: "/haha1",
+    slug: "haha1",
     thumbnail: {
       url: "https://images.unsplash.com/photo-1579400628679-baa28dadbd54",
       alt: "s2",
-      dimensions: {
-        width: 1920,
-        height: 1080,
-      },
     },
   },
   {
     headline: "Final Fantasy 14: Bardam’s Mettle Dungeon Guide",
-    url: "/haha1",
+    slug: "haha1",
     thumbnail: {
       url: "https://images.unsplash.com/photo-1605804097616-ed12e891e514",
       alt: "s3",
-      dimensions: {
-        width: 1920,
-        height: 1080,
-      },
     },
   },
   {
     headline: "So, What Games Are Women Supposed To Play, Exactly?",
-    url: "/haha1",
+    slug: "haha1",
     thumbnail: {
       url: "https://images.unsplash.com/photo-1609365410860-bdd108741ef5",
       alt: "s4",
-      dimensions: {
-        width: 1920,
-        height: 1080,
-      },
     },
   },
 ];
@@ -75,7 +59,7 @@ const LatestArticles = ({ _articlesData }) => {
           articlesData.map((articleData, index) => {
             return (
               <article key={index}>
-                <Link href={articleData.url}>
+                <Link href={`/articles/${articleData.slug}`}>
                   <a className="decoration-violet-600 decoration-2 underline-offset-2 hover:underline dark:decoration-violet-400">
                     <figure>
                       <Image
