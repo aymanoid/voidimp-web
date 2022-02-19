6791561213202223b5918ee152fa81f195700d7f
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-19 07:08:10 +0100

    uncenter the list

--- components/home/LatestArticles.jsx
+++ components/home/LatestArticles.jsx
@@ -50,6 +50,194 @@ const articlesData = [
     categoryName: "News",
     postDate: "2022-01-22T07:04:45+0000",
   },
+  {
+    headline: "TikTok Cosplayer Yandere Freak Charged With Manslaughter",
+    subheadline: "Activision Blizzard games won't be Xbox exclusives just yet.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1615961943966-ab7585d5bf4e",
+      alt: "s1",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline:
+      "Black Myth: Wukong New Year Trailer Reveals Cats Can Be Difficult Mo-Cap Actors",
+    subheadline:
+      "The next Assassin's Creed game could be much smaller in scope than we're used to.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1579400628679-baa28dadbd54",
+      alt: "s2",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "Final Fantasy 14: Bardam’s Mettle Dungeon Guide",
+    subheadline:
+      "As Game Pass becomes even more powerful, indie devs are worried they'll have no choice but to be on it.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1605804097616-ed12e891e514",
+      alt: "s3",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "So, What Games Are Women Supposed To Play, Exactly?",
+    subheadline: "It's a good time to be a Gen 4 fan.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1609365410860-bdd108741ef5",
+      alt: "s4",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "TikTok Cosplayer Yandere Freak Charged With Manslaughter",
+    subheadline: "Activision Blizzard games won't be Xbox exclusives just yet.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1615961943966-ab7585d5bf4e",
+      alt: "s1",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline:
+      "Black Myth: Wukong New Year Trailer Reveals Cats Can Be Difficult Mo-Cap Actors",
+    subheadline:
+      "The next Assassin's Creed game could be much smaller in scope than we're used to.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1579400628679-baa28dadbd54",
+      alt: "s2",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "Final Fantasy 14: Bardam’s Mettle Dungeon Guide",
+    subheadline:
+      "As Game Pass becomes even more powerful, indie devs are worried they'll have no choice but to be on it.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1605804097616-ed12e891e514",
+      alt: "s3",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "So, What Games Are Women Supposed To Play, Exactly?",
+    subheadline: "It's a good time to be a Gen 4 fan.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1609365410860-bdd108741ef5",
+      alt: "s4",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "TikTok Cosplayer Yandere Freak Charged With Manslaughter",
+    subheadline: "Activision Blizzard games won't be Xbox exclusives just yet.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1615961943966-ab7585d5bf4e",
+      alt: "s1",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline:
+      "Black Myth: Wukong New Year Trailer Reveals Cats Can Be Difficult Mo-Cap Actors",
+    subheadline:
+      "The next Assassin's Creed game could be much smaller in scope than we're used to.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1579400628679-baa28dadbd54",
+      alt: "s2",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "Final Fantasy 14: Bardam’s Mettle Dungeon Guide",
+    subheadline:
+      "As Game Pass becomes even more powerful, indie devs are worried they'll have no choice but to be on it.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1605804097616-ed12e891e514",
+      alt: "s3",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "So, What Games Are Women Supposed To Play, Exactly?",
+    subheadline: "It's a good time to be a Gen 4 fan.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1609365410860-bdd108741ef5",
+      alt: "s4",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "TikTok Cosplayer Yandere Freak Charged With Manslaughter",
+    subheadline: "Activision Blizzard games won't be Xbox exclusives just yet.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1615961943966-ab7585d5bf4e",
+      alt: "s1",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline:
+      "Black Myth: Wukong New Year Trailer Reveals Cats Can Be Difficult Mo-Cap Actors",
+    subheadline:
+      "The next Assassin's Creed game could be much smaller in scope than we're used to.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1579400628679-baa28dadbd54",
+      alt: "s2",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "Final Fantasy 14: Bardam’s Mettle Dungeon Guide",
+    subheadline:
+      "As Game Pass becomes even more powerful, indie devs are worried they'll have no choice but to be on it.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1605804097616-ed12e891e514",
+      alt: "s3",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
+  {
+    headline: "So, What Games Are Women Supposed To Play, Exactly?",
+    subheadline: "It's a good time to be a Gen 4 fan.",
+    url: "/haha1",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1609365410860-bdd108741ef5",
+      alt: "s4",
+    },
+    categoryName: "News",
+    postDate: "2022-01-22T07:04:45+0000",
+  },
 ];
 
 const formatDate = (timestamp, locale) => {
@@ -78,7 +266,7 @@ const LatestArticles = ({ _articlesData }) => {
   }[locale];
 
   return (
-    <div className="mx-auto mt-12 max-w-4xl">
+    <div className="mt-20 max-w-4xl">
       <h2 className="text-xl font-bold uppercase text-violet-600 shadow-violet-400/40 drop-shadow-lg dark:text-violet-400">
         {`${strings.latest}`}
       </h2>
