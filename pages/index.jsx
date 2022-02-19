4b8b17e506eae5d741dbb6d2d49992affd55e7cf
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-19 06:40:41 +0100

    fix divider line for arabic

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,8 +10,8 @@ const Home = ({ globalData, homePageData }) => {
   return (
     <Layout globalData={globalData}>
       <div className="container mx-auto min-h-0 max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-        <div className="-mx-8 flex flex-wrap divide-x divide-violet-600/50 overflow-hidden dark:divide-violet-400/50 md:flex-row">
-          <section className="flex w-full flex-col px-8 md:w-3/5">
+        <div className="-mx-8 flex flex-wrap md:flex-row">
+          <section className="flex w-full flex-col border-violet-600/50 px-8 ltr:border-r rtl:border-l dark:border-violet-400/50 md:w-3/5">
             <HeroArticle articleData={homePageData.heroArticle} />
             <div className="mt-4 border-b border-black/50 dark:border-white/50" />
             <SocialAd />
