1f50158c643bac949dc5578b9d52f705241e2ec2
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-19 04:20:34 +0100

    improve color a bit

--- components/home/SocialAd.jsx
+++ components/home/SocialAd.jsx
@@ -18,7 +18,7 @@ const SocialAd = () => {
     <div className="mt-4 flex justify-center">
       <Link href="/">
         <a className="grid h-[100px] w-[320px] place-items-center place-self-end rounded-xl bg-[#1DA1F2] transition duration-200 hover:brightness-110">
-          <span className="inline-flex font-bold text-black dark:text-black">
+          <span className="inline-flex font-bold text-black dark:text-neutral-800">
             <TwitterSvg
               className="inline-block h-6 w-6 fill-current ltr:mr-2 rtl:ml-2"
               viewBox="0 0 24 24"
