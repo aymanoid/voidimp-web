eafb2224a9a52c311a6fa2b6031d0dc0b00050fd
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-24 23:26:04 +0100

    make access key persist

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  const legit = req.nextUrl.searchParams.get("key") === "cGFzc3dvcmQ=";
+export async function middleware(req, s) {
+  const res = NextResponse.next();
+  const access_key = req.nextUrl.searchParams.get("key");
+
+  if (access_key) {
+    res.cookie("access_key", access_key, {
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 24 * 14,
+      httpOnly: true,
+      sameSite: "strict",
+    });
+  }
+
+  const legit = req.cookies["access_key"] === "cGFzc3dvcmQ=";
 
   if (process.env.MAINT_MODE === "true" && !legit) {
     return NextResponse.json(
