39fba5c9112bf7678aef103fa68d98cc8f92acbe
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-24 02:31:27 +0100

    fix maint mode not working (hopefully)

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   if (process.env.MAINT_MODE === "yes") {
-    const url = req.nextUrl.clone();
-    url.pathname = "/maint";
-
-    return NextResponse.rewrite(url);
+    return NextResponse.rewrite(new URL("/maint", req.url));
   }
 
   return NextResponse.next();
