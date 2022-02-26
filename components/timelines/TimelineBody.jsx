57df49ea4c36f7c1ee27a968552f5f49b4f95904
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-26 01:13:42 +0100

    stylize the timeline a bit and add mock data

--- components/timelines/TimelineBody.jsx
+++ components/timelines/TimelineBody.jsx
@@ -1,67 +1,107 @@
 import Image from "next/image";
 
+const timelineData = [
+  {
+    title: "Captain America: The First Avenger",
+    portion: "",
+    notes: "",
+    direction: "left",
+  },
+  {
+    title: "Agent Carter",
+    portion: "season 01",
+    notes: "",
+    direction: "right",
+  },
+  {
+    title: "Agent Carter",
+    portion: "season 02",
+    notes: "",
+    direction: "right",
+  },
+  {
+    title: "Agent Carter",
+    portion: "one-shot on Iron Man 3 DVD",
+    notes: "",
+    direction: "right",
+  },
+  {
+    title: "Captain Marvel",
+    portion: "",
+    notes: "",
+    direction: "left",
+  },
+  {
+    title: "Iron Man",
+    portion: "",
+    notes: "",
+    direction: "left",
+  },
+  {
+    title: "Iron Man 2",
+    portion: "",
+    notes: "",
+    direction: "left",
+  },
+  {
+    title: "The Incredible Hulk",
+    portion: "",
+    notes: "",
+    direction: "left",
+  },
+];
+
 const TimelineBody = ({ _timelineData }) => {
   return (
     <div className="mx-auto grid w-full max-w-7xl grid-cols-9 px-2">
-      {/* <!-- Stack 1 --> */}
-      <div className="col-span-4 h-full w-full ">
-        <div className="h-full w-full rounded-md bg-indigo-400 p-2 md:pl-4">
-          <h1 className="py-2 text-xl font-medium text-white">Title</h1>
-          <p className="text-xs text-gray-100 sm:text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
-            corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
-            quas omnis vero totam ullam, reprehenderit ratione pariatur
-            accusamus suscipit odit nostrum?
-          </p>
-        </div>
-      </div>
-      <div className="relative col-span-1 flex h-full w-full items-center justify-center">
-        <div className="h-full w-1 bg-indigo-300"></div>
-        <div className="absolute z-10 h-6 w-6 rounded-full bg-indigo-400 text-center text-white">
-          1
-        </div>
-      </div>
-      <div className="col-span-4 h-full w-full"></div>
-
-      {/* <!-- Stack 2 --> */}
-      <div className="col-span-4 h-full w-full"></div>
-      <div className="relative col-span-1 flex h-full w-full items-center justify-center">
-        <div className="h-full w-1 bg-indigo-300"></div>
-        <div className="absolute z-10 h-6 w-6 rounded-full bg-indigo-400 text-center text-white">
-          2
-        </div>
-      </div>
-      <div className="col-span-4 h-full w-full ">
-        <div className="h-full w-full rounded-md bg-indigo-400 p-2 md:pl-4">
-          <h1 className="py-2 text-xl font-medium text-white">Title</h1>
-          <p className="text-xs text-gray-100 sm:text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
-            corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
-            quas omnis vero totam ullam, reprehenderit ratione pariatur
-            accusamus suscipit odit nostrum?
-          </p>
-        </div>
-      </div>
-
-      {/* <!-- Stack 3 --> */}
-      <div className="col-span-4 h-full w-full ">
-        <div className="h-full w-full rounded-md bg-indigo-400 p-2 md:pl-4">
-          <h1 className="py-2 text-xl font-medium text-white">Title</h1>
-          <p className="text-xs text-gray-100 sm:text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
-            corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
-            quas omnis vero totam ullam, reprehenderit ratione pariatur
-            accusamus suscipit odit nostrum?
-          </p>
-        </div>
-      </div>
-      <div className="relative col-span-1 flex h-full w-full items-center justify-center">
-        <div className="h-full w-1 bg-indigo-300"></div>
-        <div className="absolute z-10 h-6 w-6 rounded-full bg-indigo-400 text-center text-white">
-          3
-        </div>
-      </div>
-      <div className="col-span-4 h-full w-full"></div>
+      {timelineData.map((e, i) => {
+        if (e.direction === "left") {
+          return (
+            <>
+              <div className="col-span-4 h-full w-full">
+                <div className="h-full w-full rounded-md p-2 md:pl-4">
+                  <h2 className="text-xl font-semibold text-black dark:text-white">
+                    {e.title}
+                  </h2>
+                  <p className="text-neutral-700 dark:text-neutral-400">
+                    {e.portion}
+                  </p>
+                </div>
+              </div>
+              <div className="relative col-span-1 flex h-full w-full items-center justify-center">
+                <div className="h-full w-1 bg-violet-600 dark:bg-violet-400"></div>
+                <div className="absolute z-10 h-6 w-6 rounded-full bg-violet-400 text-center text-black dark:bg-violet-600 dark:text-white">
+                  {i + 1}
+                </div>
+              </div>
+              <div className="col-span-4 h-full w-full"></div>
+            </>
+          );
+        }
+        if (e.direction === "right") {
+          return (
+            <>
+              <div className="col-span-4 h-full w-full"></div>
+              <div className="relative col-span-1 flex h-full w-full items-center justify-center">
+                <div className="h-full w-1 bg-violet-600 dark:bg-violet-400"></div>
+                <div className="absolute z-10 h-6 w-6 rounded-full bg-violet-400 text-center text-black dark:bg-violet-600 dark:text-white">
+                  {i + 1}
+                </div>
+              </div>
+              <div className="col-span-4 h-full w-full ">
+                <div className="h-full w-full rounded-md p-2 md:pl-4">
+                  <h2 className="text-xl font-semibold text-black dark:text-white">
+                    {e.title}
+                  </h2>
+                  <p className="text-neutral-700 dark:text-neutral-400">
+                    {e.portion}
+                  </p>
+                </div>
+              </div>
+            </>
+          );
+        }
+      })}
     </div>
   );
 };
