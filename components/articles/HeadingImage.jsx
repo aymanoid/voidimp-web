d577e9bace6fdf74dea2d7679592c07178201536
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-27 22:11:46 +0100

    use next's image component to improve lcp

--- components/articles/HeadingImage.jsx
+++ components/articles/HeadingImage.jsx
@@ -1,54 +1,20 @@
-import Imgix, { Picture, Source } from "react-imgix";
-import "lazysizes";
-import "lazysizes/plugins/attrchange/ls.attrchange";
+import Image from "next/image";
 
 const HeadingImage = ({ imageData }) => {
   return (
     <div className="flex justify-center">
-      <div className="flex w-full rounded-3xl border border-violet-600/50 p-2 dark:border-violet-400/50 lg:w-1/2">
-        <figure className="">
-          <Picture>
-            <Source
-              src={imageData.url}
-              width={640}
-              height={640 / (16 / 9)}
-              htmlAttributes={{ media: "(min-width: 1024px)" }}
-            />
-            {/* <Source
-              src={imageData.url}
-              width={768}
-              height={432}
-              htmlAttributes={{ media: "(min-width: 768px)" }}
-            /> */}
-            <Source
-              src={imageData.url}
-              width={768}
-              height={768 / (16 / 9)}
-              htmlAttributes={{ media: "(min-width: 640px)" }}
-            />
-            <Source
-              src={imageData.url}
-              width={640}
-              height={640 / (16 / 9)}
-              htmlAttributes={{ media: "(min-width: 0px)" }}
-            />
-            <Imgix
-              className="lazyload rounded-3xl"
-              src={imageData.url}
-              width={imageData.dimensions.width}
-              height={imageData.dimensions.height}
-              htmlAttributes={{ alt: imageData.alt }}
-              attributeConfig={{
-                src: "data-src",
-                srcSet: "data-srcset",
-                sizes: "data-sizes",
-              }}
-            />
-          </Picture>
+      <figure className="flex w-full rounded-3xl border border-violet-600/50 p-2 dark:border-violet-400/50 lg:w-1/2">
+        <Image
+          src={imageData.url}
+          alt={imageData.alt}
+          width={768}
+          height={768 / (16 / 9)}
+          className="rounded-3xl"
+          priority
+        />
 
-          {/* TODO: add and stylize figurecaption tag */}
-        </figure>
-      </div>
+        {/* TODO: add and stylize figurecaption tag */}
+      </figure>
     </div>
   );
 };
