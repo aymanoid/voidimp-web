c12133937c67528b4d3befa877f26a2d9045d4c1
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-03-01 00:30:04 +0100

    finally fix updater (i'm so dumb) pt. 2

--- pages/api/update-articles.js
+++ pages/api/update-articles.js
@@ -6,32 +6,23 @@ const handler = async (req, res) => {
   }
 
   if (req.body.type !== "api-update" || !req.body.documents.length) {
-    console.log("hi 1");
     return res.json({ noUpdate: true });
   }
 
   try {
-    console.log("hi 2");
-
     const prismicRes = await client.getAllByType("article");
-    console.log("hi 3");
 
     const updatedUids = prismicRes
-      .filter((e) => req.documents.includes(e.id))
+      .filter((e) => req.body.documents.includes(e.id))
       .map((e) => e.uid);
     const updatedPaths = updatedUids.map((e) => `/articles/${e}`);
     updatedPaths.push(...updatedPaths.map((e) => `/ar${e}`));
-    console.log(updatedPaths);
 
     const promises = updatedPaths.map((e) => res.unstable_revalidate(e));
-    console.log(promises);
 
     await Promise.all(promises);
     return res.json({ updated: true });
   } catch (err) {
-    console.error(err);
-    console.log("hi 4");
-
     return res.status(500).send("Error updating");
   }
 };
