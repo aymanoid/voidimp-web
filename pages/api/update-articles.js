8167388231b5b4cf022563a113bcd7f9ce9d6b28
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-02-28 22:35:27 +0100

    add webhook to update articles

---
+++ pages/api/update-articles.js
@@ -0,0 +1,29 @@
+import client from "utils/prismic";
+
+const handler = async (req, res) => {
+  if (req.body.secret !== process.env.UPDATER_SECRET_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  if (req.type !== "api-update" || !req.documents.length) {
+    return res.json({ noUpdate: true });
+  }
+
+  try {
+    const prismicRes = await client.getAllByType("article");
+    const updatedUids = prismicRes
+      .filter((e) => req.documents.includes(e.id))
+      .map((e) => e.uid);
+    const updatedPaths = updatedUids.map((e) => `/articles/${e}`);
+    updatedPaths.push(...updatedPaths.map((e) => `/ar${e}`));
+
+    const promises = updatedPaths.map((e) => res.unstable_revalidate(e));
+
+    await Promise.all(promises);
+    return res.json({ updated: true });
+  } catch (err) {
+    return res.status(500).send("Error updating");
+  }
+};
+
+export default handler;
