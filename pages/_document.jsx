b2bb7ce473d761965865ca4a301b54443f722bde
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-18 17:21:02 +0100

    add new favicon and other meta icons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,29 @@ class VoidImpDocument extends Document {
       <Html>
         <Head prefix="og: http://ogp.me/ns# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#">
           <meta name="robots" content="noindex, nofollow" />
-          <link rel="icon" href="/favicon.svg" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8b5cf6" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#1e1e1e" />
+
           <link rel="preconnect" href="https://images.prismic.io" />
           <link rel="dns-prefetch" href="https://images.prismic.io" />
           <link rel="preconnect" href="https://images.unsplash.com" />
