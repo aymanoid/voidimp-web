bb8cc3e5b4d8be2cee1c2482bbad51a9170a1f6a
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-23 04:51:55 +0100

    add api util for new api

---
+++ utils/api.js
@@ -0,0 +1,31 @@
+import qs from "qs";
+
+export const getStrapiURL = (path = "") => {
+  return `${
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
+  }${path}`;
+};
+
+export const fetchAPI = async (path, urlParamsObject = {}, options = {}) => {
+  const mergedOptions = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.CMS_API_TOKEN}`,
+    },
+    ...options,
+  };
+
+  const queryString = qs.stringify(urlParamsObject);
+  const requestUrl = `${getStrapiURL(
+    `/api${path}${queryString ? `?${queryString}` : ""}`
+  )}`;
+
+  const response = await fetch(requestUrl, mergedOptions);
+
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error(`An error occured please try again`);
+  }
+  const data = await response.json();
+  return data;
+};
