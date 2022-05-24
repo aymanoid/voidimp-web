a99061dea8a1d6e5f0c16d2be8fba5de908301e0
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-24 19:51:52 +0100

    add helper function to hash all ids of an object

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,28 @@
+import crypto from "crypto";
+
 export const urlize = (path, locale) => {
   const origin = "https://voidimp.com";
   return `${origin}${locale === "ar" ? "/ar" : ""}${path}`;
+};
+
+export const hashIds = (obj) => {
+  if (typeof obj !== "object") return obj;
+
+  for (let key in obj) {
+    if (key === "id") {
+      obj[key] = crypto
+        .createHash("shake256", { outputLength: 4 })
+        .update(obj[key].toString())
+        .digest("hex");
+    }
+    if (Array.isArray(obj[key])) {
+      for (const [index] of obj[key].entries()) {
+        obj[key][index] = hashIds(obj[key][index]);
+      }
+    } else {
+      obj[key] = hashIds(obj[key]);
+    }
+  }
+
+  return obj;
 };
