ecb2718b8102d9e6de04917f746849e8c1e5433c
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-25 23:47:34 +0100

    temporary disable search page
    disable search page for the time being as it doesn't work with the new api.

--- components/common/SearchBar.jsx
+++ components/common/SearchBar.jsx
@@ -1,21 +1,8 @@
-import { useRouter } from "next/router";
 import MagnifierIcon from "components/icons/magnifier.svg";
 
 const SearchBar = ({ searchBarLabel }) => {
-  const { locale } = useRouter();
-
-  const origin =
-    typeof window !== "undefined" && window.location.origin
-      ? window.location.origin
-      : process.env.NODE_ENV === "production"
-      ? "https://www.voidimp.com"
-      : "http://localhost:3000";
-
   return (
-    <form
-      className="relative mt-4 xl:mx-4 xl:mt-0"
-      action={`${origin}/${locale === "ar" ? "ar/" : ""}search`}
-    >
+    <form className="relative mt-4 xl:mx-4 xl:mt-0">
       <button
         className="absolute inset-y-0 left-0 flex items-center pl-3"
         type="submit"
@@ -32,7 +19,6 @@ const SearchBar = ({ searchBarLabel }) => {
         type="text"
         className="w-full rounded-full border-2 border-transparent bg-neutral-200 py-1 pl-10 pr-4 text-neutral-700 placeholder-neutral-500 transition-colors duration-200 hover:border-neutral-500 focus:border-violet-600 focus:bg-white focus:outline-none dark:bg-neutral-700 dark:text-neutral-200 dark:placeholder-neutral-400 dark:hover:border-neutral-400 dark:focus:border-violet-400 dark:focus:bg-neutral-900 xl:w-56"
         placeholder={searchBarLabel}
-        name="q"
       />
     </form>
   );
