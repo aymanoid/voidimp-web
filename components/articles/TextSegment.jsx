ce43064c87ac9b03bd4c5b5c2ef615412c8b49cc
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-26 00:30:02 +0100

    implement md to html converter for text segment

--- components/articles/TextSegment.jsx
+++ components/articles/TextSegment.jsx
@@ -1,8 +1,12 @@
+import showdown from "showdown";
+
+const markdownConverter = new showdown.Converter();
+
 const TextSegment = ({ textData }) => {
   return (
     <div
       className="prose prose-neutral max-w-none prose-a:font-semibold prose-a:decoration-violet-600 prose-a:underline-offset-2 hover:prose-a:decoration-2 dark:prose-invert dark:decoration-violet-400 dark:prose-a:decoration-violet-400 lg:prose-xl"
-      dangerouslySetInnerHTML={{ __html: textData }}
+      dangerouslySetInnerHTML={{ __html: markdownConverter.makeHtml(textData) }}
     ></div>
   );
 };
