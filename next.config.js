e31915f4f1765ad499696c88728439ebf8e0058d
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-26 20:31:03 +0100

    implement bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.BUNDLE_ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   i18n: {
     locales: ["en", "ar"],
     defaultLocale: "en",
@@ -25,4 +29,4 @@ module.exports = {
       },
     ];
   },
-};
+});
