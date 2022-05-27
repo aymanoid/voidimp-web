49ac144433b6d80874bf98184ed9e2676471b2be
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-27 01:03:04 +0100

    fix ga id env variable usage

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,6 +1,6 @@
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  window.gtag("config", NEXT_PUBLIC_GA_ID, {
+  window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
     page_path: url,
   });
 };
