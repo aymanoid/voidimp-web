d109139a3d42c56b7cec34e7b34d2ce6bb54c00f
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-06-03 12:12:00 +0100

    fix unknown prop warning

--- components/common/VoidImage.jsx
+++ components/common/VoidImage.jsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import { getCmsURL } from "utils/api";
 
 const VoidImage = (props) => {
-  const imgProps = { ...props };
+  const imgProps = Object.assign({}, props);
   imgProps.src = imgProps.cmsMedia ? getCmsURL(imgProps.src) : imgProps.src;
+  delete imgProps.cmsMedia;
   return <Image {...imgProps} />;
 };
 
